b398e1c9ca32138acc050bf58c45705d2676079a
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-03-17 21:28:00 +0100

    Added enum 'BuildingType', which represents the different building heights; updated Cell class
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

---
+++ src/main/java/it/polimi/ingsw/Model/BuildingType.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.Model;
+
+import java.util.Objects;
+
+public enum BuildingType {
+    NONE,BASE,MIDDLE,TOP,DOME;
+}
--- src/main/java/it/polimi/ingsw/Model/Cell.java
+++ src/main/java/it/polimi/ingsw/Model/Cell.java
@@ -1,17 +1,42 @@
 package it.polimi.ingsw.Model;
 
+import java.util.Objects;
+
 public class Cell {
 
-    private Builder builder;
-    private int height = 0;
-    private boolean hasDome = false;
+    // NOTA: questo metodo va quasi bene ma va inserito in Builder, non qua, poichè può essere necessario override con Dei (in particolare Atlas)
+    /*private void isValidBuild(BuildingType height) throws BuildingOnDomeException,BuildingTwoLevelsException,InvalidBuildException {
 
-    public int getHeight() {
+        if ((this.height).equals(BuildingType.DOME)) throw new BuildingOnDomeExcepion(); // verify that there is no dome on the cell
+        else if ((this.height).compareTo(height) <= 0)  throw new InvalidBuildException(); // verify that I'm not building on the same level as already present or on a lower level
+        else if ((this.height).equals(BuildingType.TOP) && !(height.equals(BuildingType.DOME))) throw new InvalidBuildException(); // if a top level is present, only a dome can be placed
+        else if ((this.height).compareTo(height) == 2) throw new BuildingTwoLevelsException(); // building two levels
+        else
+    }*/
+
+    private Builder builder;
+
+    private BuildingType height = BuildingType.NONE;
+
+    public BuildingType getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    public void setHeight(BuildingType height) {
+        try{
+            builder.isValidBuild(height);
+            this.height = height; //OK
+
+        }
+        catch(NullPointerException e) {
+
+        }
+        catch (BuildingOnDomeException e) {
+
+        }
+        catch (SameLevelBuildingException e) {
+
+        }
     }
 
     public Builder getBuilder() {
