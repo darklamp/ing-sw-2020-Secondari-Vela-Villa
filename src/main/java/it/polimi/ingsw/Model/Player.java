524888d61dc4a8d84274546a8ed8a3d2d5b9c66f
LorenzoVela <61782614+LorenzoVela@users.noreply.github.com>
2020-03-18 12:33:44 +0100

    Initialization of the Player's attributes and classes. I've added the method "MoveFrom" and a constructor for Player that also sets in automatically "true" the attribute "IsinGame"(if the Nickname is accepted).

--- src/main/java/it/polimi/ingsw/Model/Player.java
+++ src/main/java/it/polimi/ingsw/Model/Player.java
@@ -1,4 +1,45 @@
 package it.polimi.ingsw.Model;
 
 public class Player {
+
+    private String Nickname; //private attribute for the Player's ID
+
+    private boolean IsInGame; //private boolean to know if the player is still in the Game
+
+    private int InGameTurn; //private number to know the turn of the player
+
+    public Player(String Nickname) {   //contructor method for player
+        try {
+            this.Nickname = Nickname  //If the nickname is accepted,the player'll be insert in the game
+            this.IsInGame = true;
+        }
+
+        catch (NullPointerException e) {
+
+        }
+        catch(NicknameAlreadyExists e) { //two players can't choose the same nickname in the same game
+
+        }
+    }
+
+    public void UpdatePlayerStatus(){
+        this.IsInGame = false;      //why set false?Beacuse the bit is setted at the start of the game to true,so I just have to change it to false.
+    }
+
+    public boolean CanMove(){
+        //return true if the player can move,otherwise set false
+    }
+
+    public void MoveTo(cell){
+        //the cell where player wants to go
+    }
+
+    public cell MoveFrom(){
+        //return the cell before the movement
+    }
+
+    public void Build(cell){
+        //the cell where the player wants to build
+    }
+
 }
