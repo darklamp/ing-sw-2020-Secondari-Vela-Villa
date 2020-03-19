ec7b7b941e795af77e3c7784010c0e15b3599962
77simone77 <77simone77@github.com>
2020-03-19 13:50:45 +0100

    update gametable

--- src/main/java/it/polimi/ingsw/Model/GameTable.java
+++ src/main/java/it/polimi/ingsw/Model/GameTable.java
@@ -1,4 +1,25 @@
 package it.polimi.ingsw.Model;
 
 public class GameTable {
+
+    private Cell[][] Table;
+
+    public GameTable() {   //contructor method for GameTable
+
+        Table= new Cell[5][5]; //create new Table
+    }
+
+    public Cell getCell(int x,int y) {
+        try {
+            return Table[x][y]; //ritorna la cella con righa x e colonna y
+        }
+        chatch(negativeoroverthanfive e) {//se vengono inserite x o y negativi o maggiori di 5
+        }
+    }
+    public boolean CheckGameover(){
+        //return true if there is just one player in game
+    }
+    public int getPlayerIn(){
+        //ritorna il numero di player ancora in gioco
+    }
 }
