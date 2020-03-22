00083d3b29fb65bb4704e36e449bfa20802c3bb4
77simone77 <77simone77@github.com>
2020-03-22 19:32:28 +0100

    update hephaestus e demeter

--- src/main/java/it/polimi/ingsw/Model/Demeter.java
+++ src/main/java/it/polimi/ingsw/Model/Demeter.java
@@ -2,35 +2,39 @@ package it.polimi.ingsw.Model;
 
 import it.polimi.ingsw.Model.Exceptions.*;
 
-public class Demeter extends Builder{
-    private boolean firsttime=true;
+public class Demeter extends Builder {
+    private boolean firsttime = true;
     private Cell previous;
+
     public Demeter(Cell position, Player player) {
-        super(position,player);
+        super(position, player);
     }
 
 
     @Override
     public void isValidBuild(BuildingType oldheight, BuildingType newheight, Cell cell) throws AtlasException, DemeterException, HephaestusException, InvalidBuildException {
-        if(firsttime) {
+        if (firsttime) {
             super.isValidBuild(oldheight, newheight);
             verifyBuild(oldheight, newheight);
-            firsttime=false;
-            previous=cell;
+            firsttime = false;
+            previous = cell;
             throw new DemeterException();
-        }
-        else{
+        } else {
             super.isValidBuild(oldheight, newheight);
             verifyBuild(oldheight, newheight);
-            else if(cell.equals(previous)){throw new InvalidBuildException()}// non posso costruire su una cella uguale a quella precedente
+            else if (cell.equals(previous)) {
+                throw new InvalidBuildException()
+            }// non posso costruire su una cella uguale a quella precedente
             else {
                 firsttime = true; //così quando verrà richiamato il metodo isvalidbuild entrerò nel ramo if
             }
         }
     }
 
+
     @Override
     public void isValidMove(Cell finalPoint) throws MinotaurException, ApolloException, InvalidMoveException, ArtemisException, PrometeusException {
         super.isValidMove(finalPoint);
         else verifyMove(finalPoint);
     }
+}
