b167847d4376161b5a6365f09a2d75c5899ca4e0
77simone77 <77simone77@github.com>
2020-03-28 16:22:02 +0100

    update apollotest

--- src/test/java/it/polimi/ingsw/Model/ApolloTest.java
+++ src/test/java/it/polimi/ingsw/Model/ApolloTest.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.Model;
 
 import it.polimi.ingsw.Model.Exceptions.HephaestusException;
 import it.polimi.ingsw.Model.Exceptions.InvalidBuildException;
+import it.polimi.ingsw.Model.Exceptions.InvalidMoveException;
 
 class ApolloTest {
 
@@ -12,19 +13,20 @@ class ApolloTest {
         Player p2 = new Player("Pippo",g);
         Cell c1 = g.getCell(4,3);
         Cell c2 = g.getCell(4,4);
-        Cell c3 = g.getCell(4,2);
+        Cell c3 = g.getCell(4,0);
         Builder b1 = new Apollo(c1,p1);
         Builder b2 = new Apollo(c2,p2);
 
         Assertions.assertThrows(ApolloException.class, () -> {
             b1.setPosition(c2);
         });
-        Assertions.assertThrows(InvalidBuildException.class, () -> {
-            c3.setHeight(b1,BuildingType.DOME);
+        Assertions.assertThrows(InvalidMoveException.class, () -> {
+            b1.setPosition(c1);
         });
-        c3.setHeight(b1,BuildingType.MIDDLE); // no exception
-        Assertions.assertThrows(InvalidBuildException.class, () -> {
-            c3.setHeight(b1,BuildingType.DOME);
+        Assertions.assertThrows(InvalidMoveException.class, () -> {
+            b1.setPosition(c3);
         });
+        Cell c0 = g.getCell(3,3);
+        b1.setPosition(c0); // shouldn't throw
     }
 }
