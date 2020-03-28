ebdbe799bdf782db9df7e3c6677797553e20ebb9
77simone77 <77simone77@github.com>
2020-03-28 16:10:23 +0100

    update apollotest

--- src/test/java/it/polimi/ingsw/Model/ApolloTest.java
+++ src/test/java/it/polimi/ingsw/Model/ApolloTest.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw.Model;
 
-public class ApolloTest {
+import it.polimi.ingsw.Model.Exceptions.HephaestusException;
+import it.polimi.ingsw.Model.Exceptions.InvalidBuildException;
+
+class ApolloTest {
+
+    @Test
+    void isValidBuildTest() throws Exception{
+        GameTable g = new GameTable();
+        Player p1 = new Player("Giggino",g);
+        Player p2 = new Player("Pippo",g);
+        Cell c1 = g.getCell(4,3);
+        Cell c2 = g.getCell(4,4);
+        Cell c3 = g.getCell(4,2);
+        Builder b1 = new Apollo(c1,p1);
+        Builder b2 = new Apollo(c2,p2);
+
+        Assertions.assertThrows(ApolloException.class, () -> {
+            b1.setPosition(c2);
+        });
+        Assertions.assertThrows(InvalidBuildException.class, () -> {
+            c3.setHeight(b1,BuildingType.DOME);
+        });
+        c3.setHeight(b1,BuildingType.MIDDLE); // no exception
+        Assertions.assertThrows(InvalidBuildException.class, () -> {
+            c3.setHeight(b1,BuildingType.DOME);
+        });
+    }
 }
