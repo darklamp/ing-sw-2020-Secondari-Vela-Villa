d62a27b054f76db7f406ea4f15df4d71e72296be
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-03-31 17:20:35 +0200

    kush
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/test/java/it/polimi/ingsw/Model/CellTest.java
+++ src/test/java/it/polimi/ingsw/Model/CellTest.java
@@ -7,38 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class CellTest {
 
     @Test
-    void getX() {
-    }
-
-    @Test
-    void getY() {
-    }
-
-    @Test
-    void getPosition() {
-    }
-
-    @Test
-    void getHeight() {
-    }
-
-    @Test
-    void setHeight() {
-    }
-
-    @Test
     void getNear() {
-    }
-
-    @Test
-    void setBuilder() {
     }
 
     @Test
     void movableCell() {
     }
 
-    @Test
-    void getBuilder() {
-    }
 }
