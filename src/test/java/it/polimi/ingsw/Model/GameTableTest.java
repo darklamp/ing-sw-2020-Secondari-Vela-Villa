8dc478862a9194773d5b0c9c10db57d4f0952383
77simone77 <10561308@polimi.it>
2020-04-04 16:19:36 +0200

    SwapPosition

---
+++ src/test/java/it/polimi/ingsw/Model/GameTableTest.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTableTest {
+
+    @Test
+    void nextTurn() {
+    }
+
+    @Test
+    void isValidPlayer() {
+    }
+
+    @Test
+    void addPlayer() {
+    }
+}
