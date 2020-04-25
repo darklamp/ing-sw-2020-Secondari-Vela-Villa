bb69ab5fec6f7125ccc3334944ce68341ae10d7e
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-04-25 16:38:12 +0200

    Updated client code
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/Client/LineClient.java
+++ src/main/java/it/polimi/ingsw/Client/LineClient.java
@@ -20,17 +20,82 @@ public class LineClient {
         this.port = port;
     }
 
+    public Thread asyncReadFromSocket(final ObjectInputStream socketIn){
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (isActive()) {
+                        Object inputObject = socketIn.readObject();
+                        if(inputObject instanceof String){
+                            System.out.println((String)inputObject);
+                        } /*else if (inputObject instanceof Board){
+                            ((Board)inputObject).print();
+                        } */else {
+                            throw new IllegalArgumentException();
+                        }
+                    }
+                } catch (Exception e){
+                    setActive(false);
+                }
+            }
+        });
+        t.start();
+        return t;
+    }
+
+
+    private boolean active = true;
+
+    public synchronized boolean isActive(){
+        return active;
+    }
+
+    public synchronized void setActive(boolean active){
+        this.active = active;
+    }
+
+    public Thread asyncWriteToSocket(final Scanner stdin, final PrintWriter socketOut){
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (isActive()) {
+                        String inputLine = stdin.nextLine();
+                        socketOut.println(inputLine);
+                        socketOut.flush();
+                    }
+                }catch(Exception e){
+                    setActive(false);
+                }
+            }
+        });
+        t.start();
+        return t;
+    }
+
+
+
     public void startClient() throws IOException {
 
         Socket socket = new Socket(ip, port);
         System.out.println("Connection established");
         Scanner stdin = new Scanner(System.in);
-        ObjectOutputStream socketOut = new ObjectOutputStream(socket.getOutputStream());
+        PrintWriter socketOut = new PrintWriter(socket.getOutputStream());
         ObjectInputStream socketIn = new ObjectInputStream(socket.getInputStream());
-        //int status = socketIn.readInt();
-       // System.out.println("Server is in status " +status);
-        String a = socketIn.readUTF();
-        System.out.println(a);
+        try{
+            Thread t0 = asyncReadFromSocket(socketIn);
+            Thread t1 = asyncWriteToSocket(stdin, socketOut);
+            t0.join();
+            t1.join();
+        } catch(InterruptedException | NoSuchElementException e){
+            System.out.println("Connection closed from the client side");
+        } finally {
+            stdin.close();
+            socketIn.close();
+            socketOut.close();
+            socket.close();
+        }
       /*  try{
             while (true) {
                 if (status == 1) {
