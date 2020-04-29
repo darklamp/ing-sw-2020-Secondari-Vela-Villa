c5f4cad9b36336fd5e90c59141a5ae10421584f3
LorenzoVela <61782614+LorenzoVela@users.noreply.github.com>
2020-04-29 12:08:38 +0200

    Added ShowTable test.

---
+++ src/test/java/it/polimi/ingsw/Client/CLI/CLITest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.Client.CLI;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CLITest {
+
+    @Test
+    void showTable() {
+    }
+}
