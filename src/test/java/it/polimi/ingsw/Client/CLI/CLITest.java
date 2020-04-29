fd0cf399de779b3b0ab756f3bc4c66a4e014b261
77simone77 <10561308@polimi.it>
2020-04-29 15:00:43 +0200

    .

--- src/test/java/it/polimi/ingsw/Client/CLI/CLITest.java
+++ src/test/java/it/polimi/ingsw/Client/CLI/CLITest.java
@@ -1,47 +1,46 @@
 package it.polimi.ingsw.Client.CLI;
 
-import it.polimi.ingsw.Model.BuildingType;
 import it.polimi.ingsw.View.CellView;
 import org.junit.jupiter.api.Test;
+import it.polimi.ingsw.Model.BuildingType;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CLITest {
 
-    private static final BuildingType NONE ;
-    private static final BuildingType MIDDLE = ;
-    private static final BuildingType TOP = ;
-    private static final BuildingType DOME = ;
 
+
+
+    private static final CLI classecli = new CLI();
+    private static final CellView [][] Matrix = new CellView[5][5];
     @Test
     void showTable() {
-        CellView[][] Matrix = new CellView[5][5];
-        Matrix[0][0]= new CellView(NONE,0);
-        Matrix[0][1]= new CellView(MIDDLE,1);
-        Matrix[0][2]= new CellView(TOP,2);
-        Matrix[0][3]= new CellView(DOME,-1);
-        Matrix[0][4]= new CellView(DOME,-1);
-        Matrix[1][0]= new CellView(DOME,-1);
-        Matrix[1][1]= new CellView(DOME,-1);
-        Matrix[1][2]= new CellView(DOME,-1);
-        Matrix[1][3]= new CellView(DOME,-1);
-        Matrix[1][4]= new CellView(DOME,-1);
-        Matrix[2][0]= new CellView(DOME,-1);
-        Matrix[2][1]= new CellView(DOME,-1);
-        Matrix[2][2]= new CellView(DOME,-1);
-        Matrix[2][3]= new CellView(DOME,-1);
-        Matrix[2][4]= new CellView(DOME,-1);
-        Matrix[3][0]= new CellView(DOME,-1);
-        Matrix[3][1]= new CellView(DOME,-1);
-        Matrix[3][2]= new CellView(DOME,-1);
-        Matrix[3][3]= new CellView(DOME,-1);
-        Matrix[3][4]= new CellView(DOME,-1);
-        Matrix[4][0]= new CellView(DOME,-1);
-        Matrix[4][1]= new CellView(DOME,-1);
-        Matrix[4][2]= new CellView(DOME,-1);
-        Matrix[4][3]= new CellView(DOME,-1);
-        Matrix[4][4]= new CellView(DOME,-1);
-        CLI classecli = new CLI();
+        Matrix[0][0]= new CellView(BuildingType.NONE,0);
+        Matrix[0][1]= new CellView(BuildingType.MIDDLE,1);
+        Matrix[0][2]= new CellView(BuildingType.TOP,2);
+        Matrix[0][3]= new CellView(BuildingType.TOP,-1);
+        Matrix[0][4]= new CellView(BuildingType.DOME,-1);
+        Matrix[1][0]= new CellView(BuildingType.DOME,-1);
+        Matrix[1][1]= new CellView(BuildingType.TOP,-1);
+        Matrix[1][2]= new CellView(BuildingType.TOP,-1);
+        Matrix[1][3]= new CellView(BuildingType.TOP,-1);
+        Matrix[1][4]= new CellView(BuildingType.TOP,-1);
+        Matrix[2][0]= new CellView(BuildingType.NONE,-1);
+        Matrix[2][1]= new CellView(BuildingType.NONE,-1);
+        Matrix[2][2]= new CellView(BuildingType.NONE,-1);
+        Matrix[2][3]= new CellView(BuildingType.NONE,-1);
+        Matrix[2][4]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[3][0]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[3][1]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[3][2]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[3][3]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[3][4]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[4][0]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[4][1]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[4][2]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[4][3]= new CellView(BuildingType.MIDDLE,-1);
+        Matrix[4][4]= new CellView(BuildingType.MIDDLE,-1);
         classecli.showTable(Matrix);
 
 
