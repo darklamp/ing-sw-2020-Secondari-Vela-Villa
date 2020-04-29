19e0faf9be1a0b246cdb1b93cf6da1065b9d5fe9
77simone77 <77simone77@github.com>
2020-04-29 12:45:07 +0200

    test CLI creato ma non usato

--- src/test/java/it/polimi/ingsw/Client/CLI/CLITest.java
+++ src/test/java/it/polimi/ingsw/Client/CLI/CLITest.java
@@ -8,5 +8,35 @@ class CLITest {
 
     @Test
     void showTable() {
+        CellView[][] Matrix = new CellView[5][5];
+        Matrix[0][0]= new CellView(NONE,0);
+        Matrix[0][1]= new CellView(MIDDLE,1);
+        Matrix[0][2]= new CellView(TOP,2);
+        Matrix[0][3]= new CellView(DOME,-1);
+        Matrix[0][4]= new CellView(DOME,-1);
+        Matrix[1][0]= new CellView(DOME,-1);
+        Matrix[1][1]= new CellView(DOME,-1);
+        Matrix[1][2]= new CellView(DOME,-1);
+        Matrix[1][3]= new CellView(DOME,-1);
+        Matrix[1][4]= new CellView(DOME,-1);
+        Matrix[2][0]= new CellView(DOME,-1);
+        Matrix[2][1]= new CellView(DOME,-1);
+        Matrix[2][2]= new CellView(DOME,-1);
+        Matrix[2][3]= new CellView(DOME,-1);
+        Matrix[2][4]= new CellView(DOME,-1);
+        Matrix[3][0]= new CellView(DOME,-1);
+        Matrix[3][1]= new CellView(DOME,-1);
+        Matrix[3][2]= new CellView(DOME,-1);
+        Matrix[3][3]= new CellView(DOME,-1);
+        Matrix[3][4]= new CellView(DOME,-1);
+        Matrix[4][0]= new CellView(DOME,-1);
+        Matrix[4][1]= new CellView(DOME,-1);
+        Matrix[4][2]= new CellView(DOME,-1);
+        Matrix[4][3]= new CellView(DOME,-1);
+        Matrix[4][4]= new CellView(DOME,-1);
+
+
+
+
     }
 }
