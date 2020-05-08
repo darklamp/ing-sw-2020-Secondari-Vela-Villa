ea5b3b99a48de71295511192dc9d840f0995aee6
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-05-08 17:59:49 +0200

    Updated HephaestusTest
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/test/java/it/polimi/ingsw/Model/HephaestusTest.java
+++ src/test/java/it/polimi/ingsw/Model/HephaestusTest.java
@@ -1,25 +1,29 @@
 package it.polimi.ingsw.Model;
 
-import it.polimi.ingsw.Model.Exceptions.*;
+import it.polimi.ingsw.Model.Exceptions.HephaestusException;
+import it.polimi.ingsw.Model.Exceptions.InvalidBuildException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class HephaestusTest {
 
     @Test
-    void isValidBuildTest() throws Exception{
-        GameTable g = GameTable.getDebugInstance(2); g.setDebugInstance();
+    void isValidBuildTest() throws Exception {
+        GameTable g = GameTable.getDebugInstance(2);
+        g.setDebugInstance();
         Player p1 = new Player("Giggino", g, "HEPHAESTUS");
-        Cell c1 = g.getCell(4,3);
-        Cell c2 = g.getCell(4,4);
-        Builder b1 = new Hephaestus(c1,p1);
-        c2.setHeight(b1,BuildingType.BASE);
-        Assertions.assertThrows(InvalidBuildException.class, () -> {
-            c2.setHeight(b1,BuildingType.DOME);
+        Cell c1 = g.getCell(4, 3);
+        Cell c2 = g.getCell(4, 4);
+        Builder b1 = new Hephaestus(c1, p1);
+        Assertions.assertThrows(HephaestusException.class, () -> {
+            c2.setHeight(b1, BuildingType.BASE);
         });
-        c2.setHeight(b1,BuildingType.MIDDLE); // no exception
         Assertions.assertThrows(InvalidBuildException.class, () -> {
-            c2.setHeight(b1,BuildingType.DOME);
+            c2.setHeight(b1, BuildingType.DOME);
+        });
+        c2.setHeight(b1, BuildingType.MIDDLE); // no exception
+        Assertions.assertThrows(InvalidBuildException.class, () -> {
+            c2.setHeight(b1, BuildingType.DOME);
         });
     }
 }
