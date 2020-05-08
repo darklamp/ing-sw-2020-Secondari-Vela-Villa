dfff72d4ceb35204802286989fcd55ceb1e8bca4
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-05-08 18:15:45 +0200

    Updated PrometheusTest
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/test/java/it/polimi/ingsw/Model/PrometheusTest.java
+++ src/test/java/it/polimi/ingsw/Model/PrometheusTest.java
@@ -1,30 +1,30 @@
 package it.polimi.ingsw.Model;
 
-import it.polimi.ingsw.Model.Exceptions.*;
+import it.polimi.ingsw.Client.ClientState;
+import it.polimi.ingsw.Model.Exceptions.InvalidBuildException;
+import it.polimi.ingsw.Model.Exceptions.PrometheusException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 class PrometheusTest {
 
     @Test
     void isValidMoveTest() throws Exception {
-        GameTable g = GameTable.getDebugInstance(2); g.setDebugInstance();
+        Field a = Player.class.getDeclaredField("turnState");
+        a.setAccessible(true);
+        GameTable g = GameTable.getDebugInstance(2);
+        g.setDebugInstance();
         Player p1 = new Player("Giggino", g, "PROMETEUS");
         Player p2 = new Player("Matte", g, "PROMETEUS");
+        a.set(p1, ClientState.MOVEORBUILD);
         Cell c1 = g.getCell(4, 3);
         Cell c2 = g.getCell(3, 2);
         Cell c3 = g.getCell(4, 2);
-        Cell c4 = g.getCell(3, 3);
-        Cell c5 = g.getCell(2, 3);
         Builder b1 = new Prometheus(c3, p1);
-            Assertions.assertThrows(PrometheusException.class, () -> {
-                c1.mustSetHeight(BuildingType.BASE);
-                b1.setPosition(c1);
-            });
-
-        Assertions.assertThrows(InvalidBuildException.class, () -> {
-            c2.setHeight(b1,BuildingType.DOME);
-        });
+        Assertions.assertThrows(PrometheusException.class, () -> c1.setHeight(b1, BuildingType.BASE));
+        Assertions.assertThrows(InvalidBuildException.class, () -> c2.setHeight(b1, BuildingType.DOME));
 
     }
 }
