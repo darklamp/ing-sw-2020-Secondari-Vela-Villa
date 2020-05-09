a8a6bcda9e22deb0a7128d506297842566b0b09d
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-05-10 00:31:47 +0200

    Added test button
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/Client/GUI/MainWindowController.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/MainWindowController.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.Client.GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 
@@ -12,11 +14,19 @@ public class MainWindowController extends WindowController {
     TextArea textArea1;
     @FXML
     GridPane gridPaneMain;
+    @FXML
+    Button grid00;
 
     private static final MainWindowController instance = new MainWindowController();
 
     public static MainWindowController getInstance() {
         return instance;
+    }
+
+    @FXML
+    void printProva(ActionEvent event) {
+        event.consume();
+        grid00.setText("PROVA");
     }
 
 
