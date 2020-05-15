00b16fc9f0c2049e0df890b7073f6df948efdf7f
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-05-16 00:37:43 +0200

    Smol cleanup
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/ServerMain.java
+++ src/main/java/it/polimi/ingsw/ServerMain.java
@@ -4,9 +4,6 @@ import it.polimi.ingsw.Network.Server;
 
 import java.io.IOException;
 
-/**
- * Hello world!
- */
 public class ServerMain {
     public static void main(String[] args) {
         Server server;
@@ -14,7 +11,7 @@ public class ServerMain {
             server = new Server();
             server.run();
         } catch (IOException e) {
-            System.err.println("Impossible to initialize the server: " + e.getMessage() + "!");
+            System.err.println("Server init failed: " + e.getMessage() + "!");
         }
     }
 }
