ef0aaed8e2f3f987acb5e1d19683874fc4b10934
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-05-19 11:03:10 +0200

    Fixed MinotaurTest
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/Model/Minotaur.java
+++ src/main/java/it/polimi/ingsw/Model/Minotaur.java
@@ -39,17 +39,16 @@ public class Minotaur extends Builder{
      * @param finalPoint cell on which builder wants to position
      * @return true if the cell behind the occupied one is empty and valid; else false
      */
-    public boolean checkEmptyCellBehind(Cell finalPoint) throws InvalidCoordinateException {
+    boolean checkEmptyCellBehind(Cell finalPoint) throws InvalidCoordinateException {
         /* Warn : X is the row, so it really is Y, and viceversa */
-            int diffY = finalPoint.getRow() - this.getPosition().getRow();
-            int diffX = finalPoint.getColumn() - this.getPosition().getColumn();
-            if(diffY == 1) {
-                if (diffX == 1) {
-                    return finalPoint.getRow() != 4 && finalPoint.getColumn() != 4 && finalPoint.movableCell(finalPoint.getRow() + 1, finalPoint.getColumn() + 1); //OK
-                }
-                else if (diffX == -1) {
-                    return finalPoint.getRow() != 4 && finalPoint.getColumn() != 0 && finalPoint.movableCell(finalPoint.getRow() + 1, finalPoint.getColumn() - 1); //OK
-                }
+        int diffY = finalPoint.getRow() - this.getPosition().getRow();
+        int diffX = finalPoint.getColumn() - this.getPosition().getColumn();
+        if (diffY == 1) {
+            if (diffX == 1) {
+                return finalPoint.getRow() != 4 && finalPoint.getColumn() != 4 && finalPoint.movableCell(finalPoint.getRow() + 1, finalPoint.getColumn() + 1); //OK
+            } else if (diffX == -1) {
+                return finalPoint.getRow() != 4 && finalPoint.getColumn() != 0 && finalPoint.movableCell(finalPoint.getRow() + 1, finalPoint.getColumn() - 1); //OK
+            }
                 else {
                     return finalPoint.getRow() != 4 && finalPoint.movableCell(finalPoint.getRow() + 1, finalPoint.getColumn()); //OK
                 }
@@ -76,17 +75,15 @@ public class Minotaur extends Builder{
             }
     }
 
-    public Pair getCellBehind(Cell finalPoint){
+    Pair getCellBehind(Cell finalPoint) {
         int diffY = finalPoint.getRow() - this.getPosition().getRow();
         int diffX = finalPoint.getColumn() - this.getPosition().getColumn();
-        if(diffY == 1) {
+        if (diffY == 1) {
             if (diffX == 1) {
                 return new Pair(finalPoint.getRow() + 1, finalPoint.getColumn() + 1);
-            }
-            else if (diffX == -1) {
+            } else if (diffX == -1) {
                 return new Pair(finalPoint.getRow() + 1, finalPoint.getColumn() - 1);
-            }
-            else {
+            } else {
                 return new Pair(finalPoint.getRow() + 1, finalPoint.getColumn());
             }
         }
--- src/test/java/it/polimi/ingsw/Model/MinotaurTest.java
+++ src/test/java/it/polimi/ingsw/Model/MinotaurTest.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.Model;
 
 import it.polimi.ingsw.Model.Exceptions.InvalidBuildException;
-import it.polimi.ingsw.Model.Exceptions.InvalidCoordinateException;
 import it.polimi.ingsw.Model.Exceptions.InvalidMoveException;
 import it.polimi.ingsw.Model.Exceptions.MinotaurException;
 import org.junit.jupiter.api.Assertions;
@@ -72,23 +71,47 @@ class MinotaurTest {
     void getCellBehindTest() throws Exception {
         GameTable g = new GameTable(2);
         Player p1 = new Player("Giggino", g, "MINOTAUR");
-        Player p2 = new Player("Giggino2", g, "ATLAS");
+        Player p2 = new Player("Giggino2", g, "MINOTAUR");
+        Cell c = g.getCell(3, 3);
         Cell c0 = g.getCell(4, 2);
         Cell c1 = g.getCell(4, 3);
-        Cell c2 = g.getCell(4, 4);
+        Cell c2 = g.getCell(1, 1);
         Cell c3 = g.getCell(3, 2);
         Cell c4 = g.getCell(2, 2);
         Cell c5 = g.getCell(3, 1);
-        Cell c6 = g.getCell(2, 0);
-        Builder b1 = new Minotaur(c0, p1);
-        Builder b2 = new Atlas(c2, p2);
-        Assertions.assertEquals(((Minotaur) b1).getCellBehind(c1).getFirst(), 4);
-        Assertions.assertEquals(((Minotaur) b1).getCellBehind(c1).getSecond(), 4);
-        Assertions.assertEquals(((Minotaur) b1).getCellBehind(c3).getFirst(), 2);
-        Assertions.assertEquals(((Minotaur) b1).getCellBehind(c3).getSecond(), 2);
-        Assertions.assertEquals(((Minotaur) b1).getCellBehind(c5).getFirst(), 2);
-        Assertions.assertEquals(((Minotaur) b1).getCellBehind(c5).getSecond(), 0);
+        Cell c6 = g.getCell(1, 2);
+        Cell c7 = g.getCell(2, 1);
+        Cell c8 = g.getCell(3, 3);
+        Cell c9 = g.getCell(2, 3);
+        Cell c10 = g.getCell(1, 3);
+        Cell c11 = g.getCell(4, 4);
+        Minotaur b1 = new Minotaur(c0, p1);
+        Minotaur b2 = new Minotaur(c4, p2);
+        Assertions.assertEquals(b1.getCellBehind(c1).getFirst(), 4);
+        Assertions.assertEquals(b1.getCellBehind(c1).getSecond(), 4);
+        Assertions.assertEquals(b1.getCellBehind(c3).getFirst(), 2);
+        Assertions.assertEquals(b1.getCellBehind(c3).getSecond(), 2);
+        Assertions.assertEquals(b1.getCellBehind(c5).getFirst(), 2);
+        Assertions.assertEquals(b1.getCellBehind(c5).getSecond(), 0);
 
+        Assertions.assertFalse(b2.checkEmptyCellBehind(c3));
+        Assertions.assertTrue(b2.checkEmptyCellBehind(c6));
+        Assertions.assertEquals(b2.getCellBehind(c6), g.getCell(0, 2).getPosition());
+        Assertions.assertTrue(b2.checkEmptyCellBehind(c7));
+        Assertions.assertEquals(b2.getCellBehind(c7), g.getCell(2, 0).getPosition());
+        Assertions.assertTrue(b2.checkEmptyCellBehind(c2));
+        Assertions.assertEquals(b2.getCellBehind(c2), g.getCell(0, 0).getPosition());
+        Assertions.assertEquals(b2.getCellBehind(c11), g.getCell(0, 0).getPosition());
+        Assertions.assertFalse(b2.checkEmptyCellBehind(c11));
+        Assertions.assertTrue(b2.checkEmptyCellBehind(c5));
+        Assertions.assertEquals(b2.getCellBehind(c9), g.getCell(2, 4).getPosition());
+        Assertions.assertEquals(b2.getCellBehind(c3), g.getCell(4, 2).getPosition());
+        Assertions.assertEquals(b2.getCellBehind(c8), c11.getPosition());
+        Assertions.assertEquals(b2.getCellBehind(c2), g.getCell(0, 0).getPosition());
+        Assertions.assertEquals(b2.getCellBehind(c10), g.getCell(0, 4).getPosition());
+        Assertions.assertTrue(b2.checkEmptyCellBehind(c8));
+        Assertions.assertTrue(b2.checkEmptyCellBehind(c9));
+        Assertions.assertTrue(b2.checkEmptyCellBehind(c10));
 
 
     }
