dc6e16075b0d6aa63120bb91e0a87da70842925d
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-05-19 18:52:16 +0200

    Created class GameStateMessage
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

---
+++ src/main/java/it/polimi/ingsw/Network/GameStateMessage.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.Network;
+
+import it.polimi.ingsw.Client.ClientState;
+
+import java.io.Serializable;
+
+/**
+ * Message sent to all clients everytime a state change happens.
+ */
+public class GameStateMessage implements Serializable {
+    private final ClientState p1, p2, p3;
+
+    public GameStateMessage(ClientState p1, ClientState p2, ClientState p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    public ClientState get(short i) {
+        return switch (i) {
+            case 1 -> p2;
+            case 2 -> p3;
+            default -> p1;
+        };
+    }
+}
