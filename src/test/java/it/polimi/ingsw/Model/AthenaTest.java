97af2391202b1a020479569c6b381ad366e82110
77simone77 <77simone77@github.com>
2020-05-24 12:14:24 +0200

    test athena

--- src/test/java/it/polimi/ingsw/Model/AthenaTest.java
+++ src/test/java/it/polimi/ingsw/Model/AthenaTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Model;
 
+import it.polimi.ingsw.Model.Exceptions.InvalidBuildException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,23 @@ public class AthenaTest {
      * doesn't go up a level. This is also checked by this test.
      * @throws Exception should not
      */
+    @Test
+    void isValidBuildTest() throws Exception {
+        GameTable g = new GameTable(2);
+
+        Player p1 = new Player("Giggino", g, "ATHENA");
+        Cell c1 = g.getCell(4, 3);
+        Cell c2 = g.getCell(4, 4);
+        Builder b1 = new Athena(c1, p1);
+
+        Assertions.assertThrows(InvalidBuildException.class, () -> {
+            b1.isValidBuild(c2, BuildingType.DOME);
+        });
+        b1.isValidBuild(c2, BuildingType.BASE);
+        Assertions.assertThrows(InvalidBuildException.class, () -> {
+            b1.isValidBuild(c2, BuildingType.DOME);
+        });
+    }
     @Test
     void isValidMoveTest() throws Exception {
         GameTable g = new GameTable(2);
