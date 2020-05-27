d36a30e81ae443f74779cb3d6ee6991c2cbd9a98
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-05-27 16:27:20 +0200

    Should have fixed windows end of line char
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/test/java/it/polimi/ingsw/Client/CLI/CLITest.java
+++ src/test/java/it/polimi/ingsw/Client/CLI/CLITest.java
@@ -60,7 +60,9 @@ class CLITest {
         a.invoke(cli, "[asdasda//111\\\41q1q7INITs]");
         System.out.flush();
         System.setOut(old);
-        assertEquals(baos.toString(), "[asdasda//111\\\41q1q7INITs]\n");
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            assertEquals(baos.toString(), "[asdasda//111\\\41q1q7INITs]\r\n");
+        } else assertEquals(baos.toString(), "[asdasda//111\\\41q1q7INITs]\n");
 
         /* test INIT string */
         Field b = Client.class.getDeclaredField("playerIndex");
@@ -87,31 +89,35 @@ class CLITest {
         System.setOut(ps);
         m.invoke(cli, ClientState.WAIT);
         System.out.flush();
-        assertEquals(baos.toString(), "Waiting for turn...\n");
+        String endLineChar = "\n";
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            endLineChar = "\r\n";
+        }
+        assertEquals(baos.toString(), "Waiting for turn..." + endLineChar);
         baos.reset();
         m.invoke(cli, ClientState.MOVE);
         System.out.flush();
-        assertEquals(baos.toString(), "It's your turn! Please choose a cell to move to and which builder to use (x,y,b): \n");
+        assertEquals(baos.toString(), "It's your turn! Please choose a cell to move to and which builder to use (x,y,b): " + endLineChar);
         baos.reset();
         m.invoke(cli, ClientState.MOVEORBUILD);
         System.out.flush();
-        assertEquals(baos.toString(), "It's your turn, You can choose whether to move(m) or build(b). Please choose: \n");
+        assertEquals(baos.toString(), "It's your turn, You can choose whether to move(m) or build(b). Please choose: " + endLineChar);
         baos.reset();
         m.invoke(cli, ClientState.BUILD);
         System.out.flush();
-        assertEquals(baos.toString(), "Please choose a cell to build on and which builder to use (x,y,b): \n");
+        assertEquals(baos.toString(), "Please choose a cell to build on and which builder to use (x,y,b): " + endLineChar);
         baos.reset();
         m.invoke(cli, ClientState.WIN);
         System.out.flush();
-        assertEquals(baos.toString(), "Hurray! You won the game!\n");
+        assertEquals(baos.toString(), "Hurray! You won the game!" + endLineChar);
         baos.reset();
         m.invoke(cli, ClientState.LOSE);
         System.out.flush();
-        assertEquals(baos.toString(), "Looks like you've lost the game.\n");
+        assertEquals(baos.toString(), "Looks like you've lost the game." + endLineChar);
         baos.reset();
         m.invoke(cli, ClientState.BUILDORPASS);
         System.out.flush();
-        assertEquals(baos.toString(), "You can choose whether to build(b) or pass(p). Please choose: \n");
+        assertEquals(baos.toString(), "You can choose whether to build(b) or pass(p). Please choose: " + endLineChar);
 
     }
 }
