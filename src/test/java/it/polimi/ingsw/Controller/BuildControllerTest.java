d11f562bcdca937bbe6a6512638f95f9b12dc293
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-06-01 19:54:22 +0200

    Added cases
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/test/java/it/polimi/ingsw/Controller/BuildControllerTest.java
+++ src/test/java/it/polimi/ingsw/Controller/BuildControllerTest.java
@@ -6,6 +6,7 @@ import it.polimi.ingsw.Model.Exceptions.InvalidBuildException;
 import it.polimi.ingsw.Model.Exceptions.InvalidCoordinateException;
 import it.polimi.ingsw.Network.Server;
 import it.polimi.ingsw.Network.SocketClientConnection;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
@@ -100,5 +101,94 @@ class BuildControllerTest {
         assertSame(BuildingType.BASE, gameTable.getCell(0, 2).getHeight());
 
 
+    }
+
+    @Test
+    void edgeCasesTest() throws Exception {
+        Field a = GameTable.class.getDeclaredField("news");
+        a.setAccessible(true);
+        GameTable gameTable = new GameTable(2);
+
+        ArrayList<Integer> choices = new ArrayList<>();
+        choices.add(1);
+        choices.add(2);
+        Player player1 = new Player("gigi", gameTable, new SocketClientConnection(new Socket(), new Server()));
+        Player player2 = new Player("gigi2", gameTable, new SocketClientConnection(new Socket(), new Server()));
+        player1.setGod(choices.get(0));
+        player2.setGod(choices.get(1));
+        try {
+            player1.initBuilderList(gameTable.getCell(2, 2));
+            player1.initBuilderList(gameTable.getCell(2, 3));
+            player2.initBuilderList(gameTable.getCell(1, 2));
+            player2.initBuilderList(gameTable.getCell(1, 1));
+        } catch (InvalidBuildException | InvalidCoordinateException e) {
+            e.printStackTrace();
+        }
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        gameTable.setPlayers(players);
+        Field b = Player.class.getDeclaredField("turnState");
+        Field d = Player.class.getDeclaredField("connection");
+        d.setAccessible(true);
+        News news = new News("ASD", (SocketClientConnection) d.get(player2));
+        news.setCoords(4, 4, 0);
+        a.set(gameTable, news);
+        b.setAccessible(true);
+        b.set(player2, player2.getFirstState());
+
+        BuildController buildController = new BuildController(gameTable);
+        Field f2 = Player.class.getDeclaredField("builderList");
+        f2.setAccessible(true);
+        Builder b2 = ((ArrayList<Builder>) f2.get(player2)).get(1);
+        Field f3 = GameTable.class.getDeclaredField("currentBuilder");
+        f3.setAccessible(true);
+        f3.set(gameTable, b2);
+        buildController.handleBuild(news);
+        Field type = GameTable.class.getDeclaredField("type");
+        type.setAccessible(true);
+        Assertions.assertEquals("BUILDKO", type.get(gameTable));
+    }
+
+    @Test
+    void buildBeforeMoveTest() throws Exception {
+        Field a = GameTable.class.getDeclaredField("news");
+        a.setAccessible(true);
+        GameTable gameTable = new GameTable(2);
+
+        ArrayList<Integer> choices = new ArrayList<>();
+        choices.add(1);
+        choices.add(8);
+        Player player1 = new Player("gigi", gameTable, new SocketClientConnection(new Socket(), new Server()));
+        Player player2 = new Player("gigi2", gameTable, null);
+        player1.setGod(choices.get(0));
+        player2.setGod(choices.get(1));
+        try {
+            player1.initBuilderList(gameTable.getCell(2, 2));
+            player1.initBuilderList(gameTable.getCell(2, 3));
+            player2.initBuilderList(gameTable.getCell(1, 2));
+            player2.initBuilderList(gameTable.getCell(1, 1));
+        } catch (InvalidBuildException | InvalidCoordinateException e) {
+            e.printStackTrace();
+        }
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        gameTable.setPlayers(players);
+
+        Field d = Player.class.getDeclaredField("connection");
+        d.setAccessible(true);
+        News news = new News("BUILD", (SocketClientConnection) d.get(player2));
+        BuildController buildController = new BuildController(gameTable);
+        Field f2 = Player.class.getDeclaredField("builderList");
+        f2.setAccessible(true);
+        Builder b2 = ((ArrayList<Builder>) f2.get(player2)).get(1);
+        Field f3 = GameTable.class.getDeclaredField("currentBuilder");
+        f3.setAccessible(true);
+        f3.set(gameTable, null);
+        assert gameTable.getCurrentBuilder() == null;
+        news.setCoords(0, 1, 1);
+        buildController.handleBuild(news);
+        Assertions.assertSame(gameTable.getCurrentBuilder(), ((ArrayList<Builder>) f2.get(player2)).get(1));
     }
 }
