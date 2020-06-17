56c27bebc2fa9f4b1a7223eaf5deb033b113ea49
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-06-17 12:45:04 +0200

    Added test case
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/test/java/it/polimi/ingsw/Model/ApolloTest.java
+++ src/test/java/it/polimi/ingsw/Model/ApolloTest.java
@@ -35,6 +35,29 @@ class ApolloTest {
     }
 
     @Test
+    void executeMoveTest() throws Exception {
+        GameTable g = new GameTable(2);
+        Player p1 = new Player("Giggino", g, "APOLLO");
+        Player p2 = new Player("Pippo", g, "ATLAS");
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(p2);
+        players.add(p1);
+        g.setPlayers(players);
+        Cell c1 = g.getCell(4, 3);
+        Cell c2 = g.getCell(4, 4);
+        Cell c3 = g.getCell(4, 0);
+        Cell c4 = g.getCell(4, 2);
+        p1.initBuilderList(c1);
+        p2.initBuilderList(c2);
+        p1.initBuilderList(c4);
+        Builder b1 = p1.getBuilderList().get(0);
+        Builder b2 = p2.getBuilderList().get(0);
+        Builder b3 = p1.getBuilderList().get(1);
+        Assertions.assertDoesNotThrow(() -> b1.executeMove(c2));
+        assert c2.getBuilder() == b1 && c1.getBuilder() == b2;
+    }
+
+    @Test
     void isValidMoveTest() throws Exception {
         GameTable g = new GameTable(2);
         Player p1 = new Player("Giggino", g, "APOLLO");
